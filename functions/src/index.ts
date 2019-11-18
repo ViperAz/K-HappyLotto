0e48388c232560ba73369eccd7bebcc46dccab66
act <kanutm.s@ku.th>
2019-11-18 18:00:53 +0700

    init repo

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from 'firebase-functions';
+import * as line from '@line/bot-sdk'
+// import * as express from 'express'
+
+
+
+const Client = new line.Client({channelAccessToken : '8YPInaLsR0Ihyte/TVBrOg7NPmBE8VTghj4ctBqZ4D7ovuBcFAjYpIRhbbWisppI2juj7MJSAiAkJaIDs+0QvwXFTwkHkjFbrxgPaoFgVK4NY9t5tD3zwvnkbcCk62DmWDwT68EOoyiEIVV9RL31DQdB04t89/1O/w1cDnyilFU='})
+
+
+// const app = express()
+
+
+const lineApiTest = functions.https.onRequest((req,res) =>{
+
+    Client.replyMessage("U2d493044794b863dee491bfc35596921",{type :"text", text : "eiei"})
+
+    console.log("EIEIEIEIEIEEI")
+
+    res.status(200).end()
+})
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+export {
+    lineApiTest
+}
