8db48339606ec6c4de469343525fb3e5c926ca19
act <kanutm.s@ku.th>
2019-11-21 17:49:52 +0700

    update check binary image

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,97 +1,81 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
 import * as line from '@line/bot-sdk'
-import * as express from 'express'
+import  * as express from 'express'
+import * as requestPromise from "request-promise"
+import { Readable } from 'stream';
+import cors = require('cors');
 
-/**
- * Dialogflow ? nahh who need dialogflow when we have k-happyFlow
- * 
- */
-
- const replyMsg = {
-     default : "ขอโทษฮะพิมอะไรไม่รู้เรื่องเลย"
- }
 const Client = new line.Client({channelAccessToken : '8YPInaLsR0Ihyte/TVBrOg7NPmBE8VTghj4ctBqZ4D7ovuBcFAjYpIRhbbWisppI2juj7MJSAiAkJaIDs+0QvwXFTwkHkjFbrxgPaoFgVK4NY9t5tD3zwvnkbcCk62DmWDwT68EOoyiEIVV9RL31DQdB04t89/1O/w1cDnyilFU='})
 
-const cors = require('cors')
 const app = express()
 
 app.use(cors({origin : true}))
 
-app.post('*', (req,res)=> {
-    console.log("AM IN")
-    console.log(req.body.events[0].text)
-
-
-    switch (req.body.events[0].text) {
-        case "สมัครสมาชิก":
-        case "register" :
-            //triggered regis mock up function
-
-        case "ตรวจหวย" :
-        case "" :
-
-        default : 
-            Client.replyMessage(req.body.events[0].replyToken,{type :"text", text : replyMsg.default})
-            .then ( () =>{
+app.post('*', (req : express.Request,res : express.Response) =>{
+    let event : line.WebhookEvent = req.body.events[0]
+    if (event.type === "message" ) {
+        if (event.message.type === "text"){
+            //call dialogflow request
+            sendToDialogflow(req)
+            .then( () => {
                 res.status(200).end()
+            })
+            .catch( err => {
+                res.send(err).status(500).end()
+            })
+        }
+        else if (event.message.type === "image"){
+
+            getImageContent(event.message.id)
+            .then((stream : Readable) =>{
+                let data : string = ""
+                stream.on("data",(chunk)=>{
+                    console.log(chunk)
+                    data += chunk
+                })
+
+                stream.on("end",()=>{
+                    console.log(data)
+                })
+
+                
             })
             .catch( err =>{
                 console.error(err)
                 res.send(err).status(500).end()
             })
-    }
+            // Client.replyMessage(req.body.events[0].replyToken,{type : "text", text : imageBinary})
+        }
 
-    
-    Client.replyMessage(req.body.events[0].replyToken,{type :"text", text : req.body.events[0].message.text})
-    .then( () =>{
-        console.log("OK ?????????????????????????????")
-        res.status(200).end()
-    })
-    .catch( err =>{
-        console.error(err)
-        res.send(err).status(500).end()
-    })
-    // res.status(404).end()
-}   
-)
-
-// const lineApiGateWay = functions.https.onRequest((req,res) =>{
-
-//     let data = req.body.events[0].message.
-
-//     if (req.method == 'POST'){
+      } else {
+        //Handle Image etc.. cases
+        reply(req)
+        .then( () => {
+            res.status(200).end()
+        })
+        .catch( err => {
+            res.send(err).status(500).end()
+        })
+      }
+})
 
 
-        
+const getImageContent = (contentId : string) => {
+    return Client.getMessageContent(contentId)
+}
 
-//     }
 
-//     Client.pushMessage("U2d493044794b863dee491bfc35596921",{type :"text", text : "eiei"})
-//     .then( () =>{
-//         console.log("Kwai")
-//     }
+const sendToDialogflow = (req : express.Request) => {
+req.headers.host = "bots.dialogflow.com";
+return requestPromise.post({
+    uri: "https://bots.dialogflow.com/line/2d24edaf-4931-42d0-acfd-a07621b67a93/webhook",
+    headers: req.headers,
+    body: JSON.stringify(req.body)
+})
+}
 
-//     )
-//     .catch( err => {
-//         console.error(err)
-//     })
-    
-
-//     console.log("EIEIEIEIEIEEI")
-
-//     res.status(200).end()
-// })
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const reply = (req : express.Request) => {
+    return Client.replyMessage(req.body.events[0].replyToken,{type : "text", text : JSON.stringify(req.body)});
+}
 
 exports.lineApiGateWay = functions.https.onRequest(app)
-
-
-// export {
-//     lineApiTest
-// }
